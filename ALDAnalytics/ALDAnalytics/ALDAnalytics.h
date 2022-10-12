9ffcd8bb00c2811e7a7a29099f34f7abe7642381
张敏超🍎 <zhangminchao@sensorsdata.cn>
2022-10-12 11:48:55 +0800

    initial

---
+++ ALDAnalytics/ALDAnalytics/ALDAnalytics.h
@@ -0,0 +1,18 @@
+//
+//  ALDAnalytics.h
+//  ALDAnalytics
+//
+//  Created by MC on 2022/10/12.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ALDAnalytics.
+FOUNDATION_EXPORT double ALDAnalyticsVersionNumber;
+
+//! Project version string for ALDAnalytics.
+FOUNDATION_EXPORT const unsigned char ALDAnalyticsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ALDAnalytics/PublicHeader.h>
+
+
